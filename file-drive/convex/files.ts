da04f8ec9d1b1a5a16d7388e5a9c25017a516dbc
yagitgadhok <gadhokyagit@gmail.com>
2024-10-05 19:30:47 +0530

    initial setup done

---
+++ file-drive/convex/files.ts
@@ -0,0 +1,32 @@
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const createFile = mutation({
+  args: {
+    name: v.string(),
+  },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("User not logged in");
+    }
+
+    await ctx.db.insert("files", {
+      name: args.name,
+    });
+  },
+});
+
+export const getFiles = query({
+  args: {},
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      return [];
+    }
+
+    return await ctx.db.query("files").collect();
+  },
+});
--- file-drive/src/app/page.tsx
+++ file-drive/src/app/page.tsx
@@ -1,10 +1,43 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  SignedIn,
+  SignOutButton,
+  SignInButton,
+  SignedOut,
+} from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Hello World</Button>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+
+      <Button
+        onClick={() => {
+          createFile({ name: "yagit" });
+        }}
+      >
+        Click Me
+      </Button>
     </div>
   );
 }
